f9493c663baa3d15940495dfd274abe878483587
Minxzhu <Evanda.yoland58@gmail.com>
2021-01-09 04:41:11 +0700

    --

--- app/src/main/java/com/example/lesprivatee3/HomeMenu.java
+++ app/src/main/java/com/example/lesprivatee3/HomeMenu.java
@@ -2,12 +2,18 @@ package com.example.lesprivatee3;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
+
+import com.google.firebase.auth.FirebaseAuth;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class HomeMenu extends AppCompatActivity{
+
+    private Button profile;
 
 
 
@@ -16,6 +22,17 @@ public class HomeMenu extends AppCompatActivity{
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_menu);
 
-        startActivity(new Intent(HomeMenu.this, LoginActivity.class));
+        profile = (Button) findViewById(R.id.Detail);
+
+
+        profile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                FirebaseAuth.getInstance().signOut();                       //perbaikan
+                startActivity(new Intent(HomeMenu.this, LoginActivity.class));
+            }
+        });
+
+//        startActivity(new Intent(HomeMenu.this, LoginActivity.class));
     }
 }
--- app/src/main/java/com/example/lesprivatee3/LoginActivity.java
+++ app/src/main/java/com/example/lesprivatee3/LoginActivity.java
@@ -3,8 +3,13 @@ package com.example.lesprivatee3;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.net.ConnectivityManager;
 import android.os.Bundle;
+import android.util.Log;
 import android.util.Patterns;
 import android.view.View;
 import android.webkit.WebView;
@@ -14,6 +19,10 @@ import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
@@ -21,106 +30,201 @@ import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
-public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
+import org.json.JSONException;
+import org.json.JSONObject;
 
-    private TextView register, forgotPassword;
-    private EditText editTextEmail, editTextPassword;
-    private Button sigIn;
+import java.sql.DatabaseMetaData;
+import java.util.HashMap;
+import java.util.Map;
 
-    private FirebaseAuth mAuth;
-    private ProgressBar progressBar;
+public class LoginActivity extends AppCompatActivity {
+
+    ProgressDialog pDialog;
+    Button btn_register, btn_login;
+    EditText txt_username, txt_password;
+    Intent intent;
+
+    int success;
+    ConnectivityManager conMgr;
+
+    private String url = "localhost/guru/php";
+
+    private static final String TAG = LoginActivity.class.getSimpleName();
+
+    private static final String TAG_SUCCESS = "success";
+    private static final String TAG_MESSAGE = "message";
+
+    public final static String TAG_USERNAME = "username";
+    public final static String TAG_ID = "id";
+
+    String tag_json_obj = "json_obj_req";
+
+    SharedPreferences sharedpreferences;
+    Boolean session = false;
+    String id, username;
+    public static final String my_shared_preferences = "my_shared_preferences";
+    public static final String session_status = "session_status";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        register = (TextView) findViewById(R.id.register);
-        register.setOnClickListener(this);
-
-        sigIn = (Button) findViewById(R.id.sigIn);
-        sigIn.setOnClickListener(this);
-
-        editTextEmail = (EditText) findViewById(R.id.email);
-        editTextPassword = (EditText) findViewById(R.id.password);
-
-        progressBar = (ProgressBar) findViewById(R.id.progressBar);
-
-        mAuth = FirebaseAuth.getInstance();
-
-        forgotPassword = (TextView) findViewById(R.id.forgotPassword);
-        forgotPassword.setOnClickListener(this);
-    }
-
-    @Override
-    public void onClick(View view) {
-        switch (view.getId()){
-            case R.id.register:
-                startActivity(new Intent(this, RegisterActivity.class));
-                break;
-
-            case R.id.sigIn:
-                userLogin();
-                break;
-
-            case R.id.forgotPassword:
-                startActivity(new Intent(this, ForgotPassword.class));
-                break;
-        }
-    }
-
-    private void userLogin() {
-        String email = editTextEmail.getText().toString().trim();
-        String password = editTextPassword.getText().toString().trim();
-
-        if(email.isEmpty()){
-            editTextEmail.setError("Email is required!");
-            editTextEmail.requestFocus();
-            return;
+        conMgr = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        {
+            if (conMgr.getActiveNetworkInfo() != null
+                    && conMgr.getActiveNetworkInfo().isAvailable()
+                    && conMgr.getActiveNetworkInfo().isConnected()) {
+            } else {
+                Toast.makeText(getApplicationContext(), "No Internet Connection",
+                        Toast.LENGTH_LONG).show();
+            }
         }
 
-        if(!Patterns.EMAIL_ADDRESS.matcher(email).matches()){
-            editTextEmail.setError("Please enter a valid email!");
-            editTextEmail.requestFocus();
-            return;
+        btn_login = (Button) findViewById(R.id.sigIn);
+        btn_register = (Button) findViewById(R.id.registerUser);
+        txt_username = (EditText) findViewById(R.id.email);
+        txt_password = (EditText) findViewById(R.id.password);
+
+        // Cek session login jika TRUE maka langsung buka MainActivity
+        sharedpreferences = getSharedPreferences(my_shared_preferences, Context.MODE_PRIVATE);
+        session = sharedpreferences.getBoolean(session_status, false);
+        id = sharedpreferences.getString(TAG_ID, null);
+        username = sharedpreferences.getString(TAG_USERNAME, null);
+
+        if (session) {
+            Intent intent = new Intent(LoginActivity.this, HomeMenu.class);
+            intent.putExtra(TAG_ID, id);
+            intent.putExtra(TAG_USERNAME, username);
+            finish();
+            startActivity(intent);
         }
 
-        if(password.isEmpty()){
-            editTextPassword.setError("Password is required!");
-            editTextPassword.requestFocus();
-            return;
-        }
 
-        if(password.length() < 6){
-            editTextPassword.setError("Min password length is 6 characters!");
-            editTextPassword.requestFocus();
-            return;
-        }
+        btn_login.setOnClickListener(new View.OnClickListener() {
 
-        progressBar.setVisibility(View.VISIBLE);
-
-        mAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
             @Override
-            public void onComplete(@NonNull Task<AuthResult> task) {
+            public void onClick(View v) {
+                // TODO Auto-generated method stub
+                String username = txt_username.getText().toString();
+                String password = txt_password.getText().toString();
 
-                if (task.isSuccessful()){
-                    FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
-
-                    if (user.isEmailVerified()){
-
-                        // redirect to user profile                                profileActivity
-                        startActivity(new Intent(LoginActivity.this, HomeMenu.class));
-                    }else{
-                        user.sendEmailVerification();
-                        Toast.makeText(LoginActivity.this, "Check your email to verify your account!", Toast.LENGTH_LONG).show();
+                // mengecek kolom yang kosong
+                if (username.trim().length() > 0 && password.trim().length() > 0) {
+                    if (conMgr.getActiveNetworkInfo() != null
+                            && conMgr.getActiveNetworkInfo().isAvailable()
+                            && conMgr.getActiveNetworkInfo().isConnected()) {
+                        checkLogin(username, password);
+                    } else {
+                        Toast.makeText(getApplicationContext() ,"No Internet Connection", Toast.LENGTH_LONG).show();
                     }
-
-                }else{
-                    Toast.makeText(LoginActivity.this, "Failed to login! Please check your credentials", Toast.LENGTH_LONG).show();
+                } else {
+                    // Prompt user to enter credentials
+                    Toast.makeText(getApplicationContext() ,"Kolom tidak boleh kosong", Toast.LENGTH_LONG).show();
                 }
-
             }
         });
 
+//        registeruser.setOnClickListener(new View.OnClickListener() {
+//
+//            @Override
+//            public void onClick(View v) {
+//                // TODO Auto-generated method stub
+//                intent = new Intent(LoginActivity.this, RegisterActivity.class);
+//                finish();
+//                startActivity(intent);
+//            }
+//        });
+
     }
-}
+
+    private void checkLogin(final String username, final String password) {
+        pDialog = new ProgressDialog(this);
+        pDialog.setCancelable(false);
+        pDialog.setMessage("Logging in ...");
+        showDialog();
+
+        StringRequest strReq = new StringRequest(Request.Method.POST, url, new Response.Listener<String>() {
+
+            @Override
+            public void onResponse(String response) {
+                Log.e(TAG, "Login Response: " + response.toString());
+                hideDialog();
+
+                try {
+                    JSONObject jObj = new JSONObject(response);
+                    success = jObj.getInt(TAG_SUCCESS);
+
+                    // Check for error node in json
+                    if (success == 1) {
+                        String username = jObj.getString(TAG_USERNAME);
+                        String id = jObj.getString(TAG_ID);
+
+                        Log.e("Successfully Login!", jObj.toString());
+
+                        Toast.makeText(getApplicationContext(), jObj.getString(TAG_MESSAGE), Toast.LENGTH_LONG).show();
+
+                        // menyimpan login ke session
+                        SharedPreferences.Editor editor = sharedpreferences.edit();
+                        editor.putBoolean(session_status, true);
+                        editor.putString(TAG_ID, id);
+                        editor.putString(TAG_USERNAME, username);
+                        editor.commit();
+
+                        // Memanggil main activity
+                        Intent intent = new Intent(LoginActivity.this, HomeMenu.class);
+                        intent.putExtra(TAG_ID, id);
+                        intent.putExtra(TAG_USERNAME, username);
+                        finish();
+                        startActivity(intent);
+                    } else {
+                        Toast.makeText(getApplicationContext(),
+                                jObj.getString(TAG_MESSAGE), Toast.LENGTH_LONG).show();
+
+                    }
+                } catch (JSONException e) {
+                    // JSON error
+                    e.printStackTrace();
+                }
+
+            }
+        }, new Response.ErrorListener() {
+
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.e(TAG, "Login Error: " + error.getMessage());
+                Toast.makeText(getApplicationContext(),
+                        error.getMessage(), Toast.LENGTH_LONG).show();
+
+                hideDialog();
+
+            }
+        }) {
+
+            @Override
+            protected Map<String, String> getParams() {
+                // Posting parameters to login url
+                Map<String, String> params = new HashMap<String, String>();
+                params.put("username", username);
+                params.put("password", password);
+
+                return params;
+            }
+
+        };
+
+        // Adding request to request queue
+        AppController.getInstance().addToRequestQueue(strReq, tag_json_obj);
+    }
+
+    private void showDialog() {
+        if (!pDialog.isShowing())
+            pDialog.show();
+    }
+
+    private void hideDialog() {
+        if (pDialog.isShowing())
+            pDialog.dismiss();
+    }
+
+    }
